71fe850ac5818d86da566507e3317b14e2cd0290
chazzlee <chazz@dev.com>
2022-10-12 11:57:48 -0400

    create questions database

---
+++ import_db.sql
@@ -0,0 +1,60 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    follower_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    body TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO
+    users ('fname', 'lname')
+VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Doe'),
+    ('Ned', 'Flanders');
+
+INSERT INTO
+    questions ('title', 'body', 'author_id')
+VALUES
+    ('Ned Question', 'NED NED NED', 1),
+    ('Jane Question', 'JANE JANE JANE', 2),
+    ('Earl Question', 'MEOW MEOW MEOW', 3);
+
+
